44b697c569f24ed79ef9426a86cf8dc03633d27d
Tanvir Ahmed <ahmed.tnvr999@gmail.com>
2025-04-28 17:56:22 +0600

    Chart created.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,23 @@
+import AppAreaChart from "@/components/charts/AppAreaChart";
+import AppPieChart from "@/components/charts/AppPieChart";
+import AppBarChart from "@/components/charts/BarChart";
 import { Button } from "@/components/ui/button"
 
 export default function Home() {
   return (
-    <div>
-
+    <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-4 p-2">
+      <div className="bg-primary-foreground p-4 rounded-md xl:col-span-2">
+        <AppBarChart />
+      </div>
+      <div className="bg-primary-foreground p-4 rounded-md"></div>
+      <div className="bg-primary-foreground p-4 rounded-md">
+        <AppPieChart />
+      </div>
+      <div className="bg-primary-foreground p-4 rounded-md"></div>
+      <div className="bg-primary-foreground p-4 rounded-md xl:col-span-2">
+        <AppAreaChart />
+      </div>
+      <div className="bg-primary-foreground p-4 rounded-md"></div>
     </div>
   );
 }
